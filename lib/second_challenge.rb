c2b65c8cd8c30442a3a8cc3e52ac714520510fa3
Lucy Suddenly <suddenlylucy@gmail.com>
2018-11-17 00:11:28 +0000

    automatically backed up by learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -6,8 +6,10 @@ def second_challenge
    meat: ["chicken", "steak", "salmon"],
    grains: ["rice", "pasta"]
   }
-  groceries.values.values
-
+  ingreients = ""
+  groceries.each do |key, value|
+    ingreients << key.values
+  end
 
   
 
