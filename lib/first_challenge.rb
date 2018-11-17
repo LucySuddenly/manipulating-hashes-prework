aa1ce6c7884783b84d7a8f9d5b25d74d724d52c1
Lucy Suddenly <suddenlylucy@gmail.com>
2018-11-17 00:16:21 +0000

    Done.

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,3 +1,4 @@
+require 'pry'
 def first_challenge
   contacts = {
     "Jon Snow" => {
@@ -12,11 +13,14 @@ def first_challenge
       favorite_icecream_flavors: ["strawberry", "cookie dough", "mint chip"]
     }
   }
-
-  #your code here
-
-
-  #remember to return your newly altered contacts hash!
+  contacts.each do |key, value|
+   # binding.pry
+    value.each do |key, value|
+      if key == :favorite_icecream_flavors
+        value.delete_if {|x| x == "strawberry"}
+      end 
+    end
+  end 
   contacts
 end
 
